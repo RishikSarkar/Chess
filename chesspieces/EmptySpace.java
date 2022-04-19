42d48e9e6528114889b84f72bd9b0e3018ece7ed
Rishik Sarkar <rishiksarkar123@gmail.com>
2022-04-19 16:15:53 -0400

    Create EmptySpace.java

---
+++ chesspieces/EmptySpace.java
@@ -0,0 +1,17 @@
+package chesspieces;
+
+/**
+ * EmptySpace Object Class
+ * @author Rishik Sarkar
+ */
+public class EmptySpace extends Piece
+{
+	/**
+	 * Constructor method for an Empty Space Object.
+	 * @param value
+	 */
+	public EmptySpace(String value) 
+	{
+		super(value);
+	}
+}
